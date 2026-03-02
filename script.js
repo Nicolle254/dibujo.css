4814a75c94e60570b6af19dd8acfcc0248500dc3
Nicolle254 <n.nicolle@uniandes.edu.co>
2026-03-02 16:47:37 +0000

    pickle

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("hola");
+
+const varTitulo = document.getElementById("titulo");
+
+varTitulo.addEventListener("click", () => {
+    varTitulo.innerText = "aladin pinguin";
+    varTitulo
+})
