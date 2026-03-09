9d540d84bee929e9d8d24c3a9e93ae7bf485d777
Nicolle254 <n.nicolle@uniandes.edu.co>
2026-03-09 15:47:27 +0000

    historia

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 //console.log("hola");
 
 const varTitulo = document.getElementById("titulo");
+const varTexto = document.getElementById("texto");
+let estadoInicial = true;
+let indice = 0;
+
+const listaTexto = ["tengo hambre", "entonces bajo a buscar comida", "todo esta cerrado", "pido un domicilio", "espero 20 minutos y llega mi comidita", "me pongo feliz", "fin"];
 
 varTitulo.addEventListener("click", () => {
-    varTitulo.innerText = "aladin pinguin";
-    varTitulo
+    if (estadoInicial === true) {
+        varTitulo. innerText = "Pickle rick";
+        varTitulo.style.color = "#ad31d3";
+        varTitulo.style.backgroundColor = "white";
+} else {
+
+        varTitulo.innerText = "aladin pinguin";
+        varTitulo.style.color = "•#f21a30";
+        varTitulo.style.backgroundColor = "transparent";
+}
+estadoInicial = !estadoInicial;
+})
+
+varTexto.addEventListener("click", () => {
+    if(indice < listaTexto.length){
+    varTexto.innerText = listaTexto [indice];
+    indice = indice + 1;
+} else {
+  indice = 0;
+    }
 })
